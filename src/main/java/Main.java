7f09392ac2ed95d2c2166cbbab30587bb0cd41f7
Bastiaan Jansen <Bastiaanj7@outlook.com>
2020-12-26 10:28:36 +0100

    Changed gitignore

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Main {
+import com.eatthepath.otp.TimeBasedOneTimePasswordGenerator;
+
+import javax.crypto.KeyGenerator;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.time.Instant;
+
+
+public class Main {
+    public static void main(String[] args) throws NoSuchAlgorithmException, InvalidKeyException {
+
+        TOTPGenerator totp = new TOTPGenerator("DREERRRR");
+        HOTPGenerator hotp = new HOTPGenerator("DREERRRR");
+
+        try {
+            String code = totp.generate(2000);
+            System.out.println(code);
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        }
+    }
 }
