12cbd65096c45e154f0c59c93e7c900356e1ce0c
Bastiaan Jansen <Bastiaanj7@outlook.com>
2020-12-27 14:47:38 +0100

    Changed example app

--- src/test/java/ExampleApp.java
+++ src/test/java/ExampleApp.java
@@ -6,14 +6,12 @@ public class ExampleApp {
         String secret = "ABCDEFGHIJKLMNOP";
 
         TOTPGenerator totp = new TOTPGenerator(secret);
-        HOTPGenerator hotp = new HOTPGenerator(secret);
 
         try {
             String code = totp.generate();
             System.out.println(code);
 
-            boolean isValid = totp.verify(code);
-            System.out.println("Code is valid: " + isValid);
+            boolean isValid = totp.verify(code); // true
         } catch (IllegalStateException e) {
             e.printStackTrace();
         }
