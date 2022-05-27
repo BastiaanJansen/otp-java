c6570ffdb9387406a0f77a5b2e9d1451fb58ab33
Bastiaan Jansen <21118262+BastiaanJansen@users.noreply.github.com>
2022-05-27 10:27:35 +0200

    small code improvements

--- src/test/java/com/bastiaanjansen/otp/HOTPTest.java
+++ src/test/java/com/bastiaanjansen/otp/HOTPTest.java
@@ -125,7 +125,7 @@ class HOTPTest {
         HOTP generator = new HOTP.Builder(secret.getBytes()).build();
         URI uri = generator.getURI(10, "issuer");
 
-        assertThat( uri.toString(), is("otpauth://hotp/issuer?digits=6&counter=10&secret=" + secret + "&issuer=issuer&algorithm=SHA1"));
+        assertThat(uri.toString(), is("otpauth://hotp/issuer?digits=6&counter=10&secret=" + secret + "&issuer=issuer&algorithm=SHA1"));
     }
 
     @Test
@@ -133,7 +133,7 @@ class HOTPTest {
         HOTP generator = new HOTP.Builder(secret.getBytes()).build();
         URI uri = generator.getURI(10, "mac&cheese");
 
-        assertThat( uri.toString(), is("otpauth://hotp/mac&cheese?digits=6&counter=10&secret=" + secret + "&issuer=mac%26cheese&algorithm=SHA1"));
+        assertThat(uri.toString(), is("otpauth://hotp/mac&cheese?digits=6&counter=10&secret=" + secret + "&issuer=mac%26cheese&algorithm=SHA1"));
     }
 
 
