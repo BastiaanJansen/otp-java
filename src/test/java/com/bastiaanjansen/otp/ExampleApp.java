ad555681b0c3e93476917cb1d7af80347891a56d
Bastiaan Jansen <Bastiaanj7@outlook.com>
2023-02-11 11:56:08 +0100

    updated example app

--- src/test/java/com/bastiaanjansen/otp/ExampleApp.java
+++ src/test/java/com/bastiaanjansen/otp/ExampleApp.java
@@ -1,29 +1,33 @@
 package com.bastiaanjansen.otp;
 
+import java.time.Clock;
 import java.time.Duration;
 
 public class ExampleApp {
     public static void main(String[] args) {
 
         // Generate a secret, if you don't have one already
-//        byte[] secret = SecretGenerator.generate();
-//
-//        // Create a TOTPGenerate instance
-//        TOTPGenerator.Builder builder = new TOTPGenerator.Builder(secret);
-//        TOTPGenerator totpGenerator = builder
-//                .withPasswordLength(6)
-//                .withAlgorithm(HMACAlgorithm.SHA1)
-//                .withPeriod(Duration.ofMinutes(15))
-//                .build();
-//
-//        try {
-//            String code = totpGenerator.now();
-//            System.out.println("Generated code: " + code);
-//
-//            // To verify a codes
-//            totpGenerator.verify(code); // true
-//        } catch (IllegalStateException e) {
-//            e.printStackTrace();
-//        }
+        byte[] secret = SecretGenerator.generate();
+        Clock clock = Clock.systemUTC();
+
+        // Create a TOTPGenerate instance
+        TOTPGenerator totpGenerator = new TOTPGenerator.Builder(secret)
+                .withHOTPGenerator(builder -> {
+                    builder.withAlgorithm(HMACAlgorithm.SHA1);
+                    builder.withPasswordLength(6);
+                })
+                .withClock(clock)
+                .withPeriod(Duration.ofMinutes(15))
+                .build();
+
+        try {
+            String code = totpGenerator.now();
+            System.out.println("Generated code: " + code);
+
+            // To verify a codes
+            totpGenerator.verify(code); // true
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        }
     }
 }
